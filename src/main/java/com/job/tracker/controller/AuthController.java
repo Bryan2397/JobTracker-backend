1bbc748e62989ab3d3c9a7e81dbd7c171c1735be
Bryan2397 <bryan.mirtil@gmail.com>
2026-01-23 23:57:51 -0500

    working on verifying bcrypt password

--- src/main/java/com/job/tracker/controller/AuthController.java
+++ src/main/java/com/job/tracker/controller/AuthController.java
@@ -2,6 +2,8 @@ package com.job.tracker.controller;
 
 import com.job.tracker.dao.UserRepository;
 import com.job.tracker.entity.User;
+import com.job.tracker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,18 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private UserRepository userRepository;
+    private final UserService userService;
 
-
-    public AuthController(UserRepository userRepository){
-        this.userRepository = userRepository;
-
+    @Autowired
+    public AuthController(UserService userService){
+        this.userService = userService;
     }
 
     @PostMapping("/register")
     public String register(@RequestBody User theUser){
-
-        userRepository.save(theUser);
+        userService.save(theUser);
         return "success";
+    }
+
+    @PostMapping("/login")
+    public boolean login(@RequestBody User theUser){
+        return userService.compare(theUser);
     }
 }
