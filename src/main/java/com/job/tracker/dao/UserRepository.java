4dabe3cfba5c38d8427ccfa524c842fb3b755bad
Bryan2397 <bryan.mirtil@gmail.com>
2026-01-25 19:19:21 -0500

    used jpa to simplify writing SQL queries

--- src/main/java/com/job/tracker/dao/UserRepository.java
+++ src/main/java/com/job/tracker/dao/UserRepository.java
@@ -2,7 +2,10 @@ package com.job.tracker.dao;
 
 import com.job.tracker.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    User findByEmail(String email);
 }
