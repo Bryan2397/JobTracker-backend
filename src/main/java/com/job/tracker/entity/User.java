a97a497f342b23e6c7fd2d6dc01a4f98c2ae3351
Bryan2397 <bryan.mirtil@gmail.com>
2026-01-26 18:33:27 -0500

    added optional first and last name

--- src/main/java/com/job/tracker/entity/User.java
+++ src/main/java/com/job/tracker/entity/User.java
@@ -10,6 +10,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name", nullable = true)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = true)
+    private String lastName;
+
     @Column(name = "email", unique = true, nullable = false)
     private String email;
 
@@ -20,12 +26,22 @@ public class User {
     public User(){
 
     }
-    public User(Long id, String email, String password){
-        this.id = id;
-        this.email = email;
-        this.password = password;
+
+    public String getFirstName() {
+        return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     public String getPassword() {
         return password;
