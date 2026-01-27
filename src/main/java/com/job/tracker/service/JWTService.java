3fe45045fb314d0614da97b6d00abe4d21fe8475
Bryan Mirtil <166929786+Bryan2397@users.noreply.github.com>
2026-01-27 16:44:44 -0500

    Massive update to jwt service
    Service responsible for creating, parsing, and validating JSON Web Tokens (JWTs).

--- src/main/java/com/job/tracker/service/JWTService.java
+++ src/main/java/com/job/tracker/service/JWTService.java
@@ -1,30 +1,67 @@
 package com.job.tracker.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
+import java.util.function.Function;
+
 
 @Service
 public class JWTService {
 
-    private String key = "Testing";
+    private final String secretKey;
+
+    public JWTService(@Value("${JWT_SECRET}") String secretKey){
+        this.secretKey = secretKey;
+    }
 
     public String generateToken(String email){
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 60 * 60 * 4))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 5))
                 .signWith(getKey())
                 .compact();
     }
 
-    public Key getKey(){
-        byte[] keyBytes = Base64.getDecoder().decode(key);
+    public SecretKey getKey(){
+        byte[] keyBytes = Base64.getDecoder().decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build().parseClaimsJws(token).getBody();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String email = extractEmail(token);
+        return (email.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
 }
