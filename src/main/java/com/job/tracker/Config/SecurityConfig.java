1f2f3350e0fa3f4ea27f407849a3125e30a48a2c
Bryan2397 <bryan.mirtil@gmail.com>
2026-04-20 15:40:28 -0400

    added the render backend link to fix any CORS issues

--- src/main/java/com/job/tracker/Config/SecurityConfig.java
+++ src/main/java/com/job/tracker/Config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/h2-console/**",
+                                "https://jobtracker-backend-aodg.onrender.com",
                                 "/api/auth/register",
                                 "/api/auth/login",
                                 "/api/auth/**"
