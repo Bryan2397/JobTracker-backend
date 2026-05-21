a5899f0f43488ab45b3ca6f5a7c934af48665484
Bryan2397 <bryan.mirtil@gmail.com>
2026-05-21 09:20:03 -0400

    added localhost link to securityConfig and corsConfig

--- src/main/java/com/job/tracker/Config/SecurityConfig.java
+++ src/main/java/com/job/tracker/Config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-                "https://jobtracker-3denwyumx-bryan2397s-projects.vercel.app"
+                "https://jobtracker-3denwyumx-bryan2397s-projects.vercel.app", "http://localhost:5173"
         ));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
